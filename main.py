5e1aefccf0640a75d34a2864362f3ef062f322d4
Akraid <akraid@yandex.ru>
2022-12-17 16:05:06 +0300

    Dz

--- main.py
+++ main.py
@@ -1,40 +1,55 @@
 #!/usr/bin/python3
 
+import json
+import sys
+
 words = {}
 
-f = open('zadanie.txt')
-for i in f:
-	z = i.split("-")
-	words[z[0][:-1]] = z[1][1:-1]
+x = str(sys.argv[1])
 
-print('''
-                        ВЫБЕРИТЕ РЕЖИМ: 
-                                                        
-"1" Для рандомнго режима               "2" Для последовательного режима''')
+if x == '-f':
+	p = str(sys.argv[2])
+	f = open(p)
+	for i in f:
+		z = i.split("-")
+		words[z[0][:-1]] = z[1][1:-1]
 
-vibor = int(input())
+	fiel_name = 'wor.json'
+	with open(fiel_name, 'w', encoding = 'utf-8') as g:
+		json.dump(words, g, ensure_ascii = False)
 
-j = 0
+	fiel_name_new = 'wor.json'
+	with open(fiel_name_new, 'r') as g:
+		wor_new = json.load(g)
+	print(wor_new)
 
-if vibor == 1:
-	import random
-	for key in words.keys():
-		random_key = random.choice(list(words))
-		print(random_key, end = '')
-		if words[random_key] == input('-'):
-			print('+')
-			j += 1
-		else:
-			print('Ответ:', words[random_key])
+	print('''
+	                        ВЫБЕРИТЕ РЕЖИМ: 
+	                                                        
+	"1" Для рандомнго режима               "2" Для последовательного режима''')
 
-if vibor == 2:
-	for key in words.keys():
-		print(key, end = '')
-		otvet = input('-')
-		if words[key] == otvet:
-			print('+')
-		else:
-			print('Ответ:', words[key])
+	vibor = int(input())
 
-print(j,'из 70 повторений')
+	j = 0
 
+	if vibor == 1:
+		import random
+		for key in wor_new.keys():
+			random_key = random.choice(list(wor_new))
+			print(random_key, end = '')
+			if wor_new[random_key] == input('-'):
+				print('+')
+				j += 1
+			else:
+				print('Ответ:', wor_new[random_key])
+
+	if vibor == 2:
+		for key in wor_new.keys():
+			print(key, end = '')
+			otvet = input('-')
+			if wor_new[key] == otvet:
+				print('+')
+			else:
+				print('Ответ:', wor_new[key])
+
+	print(j,'из 70 повторений')
